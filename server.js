bd86499dca1a78ba00395ea0eefaff139b012a79
Helmut Iarley <78214599+helmutiarley@users.noreply.github.com>
2023-02-10 12:05:41 -0300

    update

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const port = 3000;
 
 const express = require('express');
 const app = express();
@@ -13,22 +14,44 @@ mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifi
     })
     .catch(e => console.log(e));
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
 const routes = require('./routes')
 const path = require('path');
-const port = 3000;
-const { middlewareGlobal } = require('./src/middlewares/middleware');
-const { novoMiddlewareGlobal } = require('./src/middlewares/middleware');
+const helmet = require('helmet');
+const csrf = require('csurf');
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
+app.use(helmet());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+const sessionOptions = session({
+    secret: 'dsadsadkamsdaskdoasdiujwqhneujac',
+    store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+});
+
+app.use(sessionOptions);
+
+app.use(flash());
 
 // Caminhos dos viwes
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
+app.use(csrf());
 // Middlewares globais
 app.use(middlewareGlobal);
-app.use(novoMiddlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
 app.use(routes);
 
 // Promise p/ conexão com banco de dados
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,9 @@
 exports.paginaInicial = (req, res, next) => {
-    res.render('index');
+    req.session.usuario = { nome: 'Luiz', logado: true };
+    res.render('index', {
+        titulo: 'Este será o título da página',
+        numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    });
     return;
 }
 
